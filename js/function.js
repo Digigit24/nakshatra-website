319bca46c75f3978748a86568b8f6a32266cd280
unknown <digigit24@gmail.com>
2026-01-13 12:31:51 +0530

    issues and console errors solved

--- js/function.js
+++ js/function.js
@@ -46,7 +46,7 @@
         slidesPerView: 1,
         speed: 1000,
         spaceBetween: 0,
-        loop: true,
+        loop: false,
         autoplay: {
             delay: 4000,
         },
@@ -63,7 +63,7 @@
             speed: 1000,
             spaceBetween: 30,
             centeredSlides: true,
-            loop: true,
+            loop: false,
             autoplay: {
                 delay: 5000,
             },
@@ -136,81 +136,6 @@
             });
         });
     }
-
-    /* Text Effect Animation */
-    if ($('.text-anime-style-1').length) {
-        let staggerAmount = 0.05,
-            translateXValue = 0,
-            delayValue = 0.5,
-            animatedTextElements = document.querySelectorAll('.text-anime-style-1');
-
-        animatedTextElements.forEach((element) => {
-            let animationSplitText = new SplitText(element, { type: "chars, words" });
-            gsap.from(animationSplitText.words, {
-                duration: 1,
-                delay: delayValue,
-                x: 20,
-                autoAlpha: 0,
-                stagger: staggerAmount,
-                scrollTrigger: { trigger: element, start: "top 85%" },
-            });
-        });
-    }
-
-    if ($('.text-anime-style-2').length) {
-        let staggerAmount = 0.03,
-            translateXValue = 20,
-            delayValue = 0.1,
-            easeType = "power2.out",
-            animatedTextElements = document.querySelectorAll('.text-anime-style-2');
-
-        animatedTextElements.forEach((element) => {
-            let animationSplitText = new SplitText(element, { type: "chars, words" });
-            gsap.from(animationSplitText.chars, {
-                duration: 1,
-                delay: delayValue,
-                x: translateXValue,
-                autoAlpha: 0,
-                stagger: staggerAmount,
-                ease: easeType,
-                scrollTrigger: { trigger: element, start: "top 85%" },
-            });
-        });
-    }
-
-    if ($('.text-anime-style-3').length) {
-        let animatedTextElements = document.querySelectorAll('.text-anime-style-3');
-
-        animatedTextElements.forEach((element) => {
-            if (element.animation) {
-                element.animation.progress(1).kill();
-                element.split.revert();
-            }
-
-            element.split = new SplitText(element, {
-                type: "lines,words,chars",
-                linesClass: "split-line",
-            });
-            gsap.set(element, { perspective: 400 });
-
-            gsap.set(element.split.chars, {
-                opacity: 0,
-                x: "50",
-            });
-
-            element.animation = gsap.to(element.split.chars, {
-                scrollTrigger: { trigger: element, start: "top 90%" },
-                x: "0",
-                y: "0",
-                rotateX: "0",
-                opacity: 1,
-                duration: 1,
-                ease: Back.easeOut,
-                stagger: 0.02,
-            });
-        });
-    }
-
     /* Parallaxie js */
     var $parallaxie = $('.parallaxie');
     if ($parallaxie.length && ($window.width() > 991)) {
